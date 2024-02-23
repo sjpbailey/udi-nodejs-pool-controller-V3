d6068c9c390e9afc5f6c0610aa70d2373183c5fe
sjpbailey <sjpbailey1961@gmail.com>
2024-02-23 00:55:33 -0800

    Update test.py

--- nodes/test.py
+++ nodes/test.py
@@ -1,5 +1,6 @@
 import asyncio
 import aiohttp
+import requests
 
 from poolcontrolpy.poolcontrolpy import Controller
 
@@ -10,5 +11,7 @@ async def checkconnection():
         controller = Controller(client, "192.168.1.53", 4200)
         return await controller.checkconnect()
 
+
 resp = asyncio.run(checkconnection())
 print(resp)
+# Get all data from nodejs pool controller api
