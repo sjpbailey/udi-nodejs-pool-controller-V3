2b395fa766d8f9aaa173985df81d3a035a006e46
sjpbailey <sjpbailey1961@gmail.com>
2024-02-24 13:51:33 -0800

    Update test.py

--- nodes/test.py
+++ nodes/test.py
@@ -5,18 +5,21 @@ import json
 x = requests.get('http://192.168.1.53:4200/config/circuits/')
 # print(json.dumps(x.json(), indent=4, sort_keys=True))
 
+# "http://192.168.1.53:4200/state/all/"
+# "http://192.168.1.53:4200/state/temps/"
+y = requests.get("http://192.168.1.53:4200/state/temps")
+# print(y.text)
+response = (json.dumps(y.json(), indent=4, sort_keys=True))
 
-y = requests.get("http://192.168.1.53:4200/state/all/")
-# print(json.dumps(y.json(), indent=4, sort_keys=True))
+print(response["air"])
 
-# print(type(y))
 
 # z = requests.get("http://192.168.1.53:4200/temperatures")
 # print(json.dumps(z.json(), indent=4, sort_keys=True))
 # print(z.text)
 
 
-a = requests.get("http://192.168.1.53:4200/config/options/pumps/")
+a = requests.get("http://192.168.1.53:4200/state/pump/1")
 # print(json.dumps(a.json(), indent=4, sort_keys=True))
 
 
@@ -25,8 +28,8 @@ u = requests.get("http://192.168.1.53:4200/state/circuit/setState/")
 # print(u.text)
 
 
-p = requests.get("http://192.168.1.53:4200/config/options/bodies/")
-print(json.dumps(p.json(), indent=4, sort_keys=True))
+p = requests.get("http://192.168.1.53:4200/config/options/schedules/")
+# print(json.dumps(p.json(), indent=4, sort_keys=True))
 # print(p.text)
-for i in p:
-    print(i[1])
+# "http://192.168.1.53:4200/config/options/schedules/"
+# "http://192.168.1.53:4200/config/options/bodies/"
