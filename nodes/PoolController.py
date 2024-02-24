8a78b42cd3452d1a4fc9054e0924fb3204be58ac
sjpbailey <sjpbailey1961@gmail.com>
2024-02-24 14:00:24 -0800

    Update PoolController.py

--- nodes/PoolController.py
+++ nodes/PoolController.py
@@ -74,7 +74,7 @@ class PoolController(udi_interface.Node):
         self.discover()
         self.poly.updateProfile()
         LOGGER.info('Starting Pool Controller')
-        try:
+        """try:
             if self.api_url:
                 self.apiBaseUrl = self.api_url
 
@@ -114,7 +114,7 @@ class PoolController(udi_interface.Node):
         except Exception as ex:
             LOGGER.error(
                 'Error reading NodeJs Pool Controller API url from Polyglot Configuration: %s', str(ex))
-            return False
+            return False"""
 
     def poll(self, flag):
         if 'longPoll' in flag:
@@ -148,23 +148,54 @@ class PoolController(udi_interface.Node):
                 else:
                     LOGGER.info('Circuit {} already configured.'.format(name))"""
 
+        try:
+            if self.api_url:
+                self.apiBaseUrl = self.api_url
+
+                # Get all data from nodejs pool controller api
+                allData = requests.get(url='{}/all'.format(self.apiBaseUrl))
+                self.allDataJson = allData.json()
+                LOGGER.info(self.allDataJson)
+                if 'circuits_not_used' in self.polyConfig['customParams']:
+
+                    # Get the list of circuits that are not in use
+                    self.circuitsNotUsed = eval(
+                        '[' + self.polyConfig['customParams']['circuits_not_used'] + ']')
+
+                    # Get circuits in use
+                    allCircuits = self.allDataJson['circuit']
+                    circuitsUsed = copy.deepcopy(allCircuits)
+                    circuitsNotUsed = self.circuitsNotUsed
+                    for key in allCircuits.keys():
+                        for circuitNotUsed in circuitsNotUsed:
+                            if key == circuitNotUsed:
+                                del circuitsUsed[key]
+
+                    self.circuits = circuitsUsed
+
+                else:
+                    self.circuits = self.allDataJson['circuit']
+
+        except Exception as ex:
+            LOGGER.error(
+                'Error reading NodeJs Pool Controller API url from Polyglot Configuration: %s', str(ex))
+            return False
             # Add pool and spa temperature nodes
-            temperatures = ['spa', 'pool']
+        temperatures = ['spa', 'pool']
 
-            for temperature in temperatures:
+        for temperature in temperatures:
 
-                temperatureData = requests.get(
-                    url='{}/temperatures'.format(self.apiBaseUrl))
-                self.temperatureDataJson = temperatureData.json()
-                LOGGER.info(self.temperatureDataJson)
-                id = temperature
-                address = ('{}_heat'.format(temperature))
-                name = ('{} Heat'.format(temperature)).title()
-                type = temperature
-
-                self.poly.addNode(TemperatureNode(
-                    self, self.address, id, address, name, type, self.temperatureDataJson, self.apiBaseUrl))
-                LOGGER.info('Temperature {} already configured.'.format(name))
+            temperatureData = requests.get(
+                url='{}/temperatures'.format(self.apiBaseUrl))
+            self.temperatureDataJson = temperatureData.json()
+            LOGGER.info(self.temperatureDataJson)
+            id = temperature
+            address = ('{}_heat'.format(temperature))
+            name = ('{} Heat'.format(temperature)).title()
+            type = temperature
+            self.poly.addNode(TemperatureNode(
+                self, self.address, id, address, name, type, self.temperatureDataJson, self.apiBaseUrl))
+            LOGGER.info('Temperature {} already configured.'.format(name))
 
     def update(self, report=True):
 
