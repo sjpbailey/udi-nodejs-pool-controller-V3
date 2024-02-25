3bea81203de18af93fc8890f5f0e7a147ca4e32e
sjpbailey <sjpbailey1961@gmail.com>
2024-02-24 22:30:37 -0800

    Update PoolController.py

--- nodes/PoolController.py
+++ nodes/PoolController.py
@@ -100,8 +100,8 @@ class PoolController(udi_interface.Node):
                 LOGGER.info("temperature 2")
                 LOGGER.info(type)
 
-                self.poly.addNode(TempNode(
-                    self, self.address, id, address, name, type, self.temperatureDataJson, self.apiBaseUrl))
+                self.poly.addNode(TempNode(self, self.address, id, address,
+                                  name, type, self.temperatureDataJson, self.apiBaseUrl))
 
                 LOGGER.info('Temperature {} already configured.'.format(name))
 
