74f1379ff210761ae5e0502dcd513aa9f0ce0c2b
sjpbailey <sjpbailey1961@gmail.com>
2024-02-27 22:04:17 -0800

    Update CircuitNode.py

--- nodes/CircuitNode.py
+++ nodes/CircuitNode.py
@@ -31,7 +31,7 @@ class CircuitNode(udi_interface.Node):
         specific node
     """
 
-    def __init__(self, polyglot, primary, address, id, name, isOn, allDataJson):
+    def __init__(self, polyglot, primary, address, name, id, isOn, allDataJson):
         """
         Optional.
         Super runs all the parent class necessities. You do NOT have
@@ -54,11 +54,7 @@ class CircuitNode(udi_interface.Node):
         self.allDataJson = allDataJson
 
     def start(self):
-        """
-        Optional.
-        This method is called after Polyglot has added the node per the
-        START event subscription above
-        """
+
         LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
         self.setDriver('ST', 1)
         LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
@@ -71,10 +67,6 @@ class CircuitNode(udi_interface.Node):
         self.http = urllib3.PoolManager()
 
     def poll(self, polltype):
-        """
-        This method is called at the poll intervals per the POLL event
-        subscription during init.
-        """
 
         if 'longPoll' in polltype:
             LOGGER.debug('longPoll (node)')
@@ -87,60 +79,30 @@ class CircuitNode(udi_interface.Node):
             LOGGER.debug('%s: get ST=%s', self.lpfx, self.getDriver('ST'))
 
     def cmd_on(self, command):
-        """
-        Example command received from ISY.
-        Set DON on TemplateNode.
-        Sets the ST (status) driver to 1 or 'True'
-        """
+
         self.setDriver('ST', 1)
 
     def cmd_off(self, command):
-        """
-        Example command received from ISY.
-        Set DOF on TemplateNode
-        Sets the ST (status) driver to 0 or 'False'
-        """
+
         self.setDriver('ST', 0)
 
     def cmd_ping(self, command):
-        """
-        Not really a ping, but don't care... It's an example to test LOGGER
-        in a module...
-        """
+
         LOGGER.debug("cmd_ping:")
         r = self.http.request('GET', "google.com")
         LOGGER.debug("cmd_ping: r={}".format(r))
 
     def query(self, command=None):
-        """
-        Called by ISY to report all drivers for this node. This is done in
-        the parent class, so you don't need to override this method unless
-        there is a need.
-        """
+
         self.reportDrivers()
 
-    """
-    Optional.
-    This is an array of dictionary items containing the variable names(drivers)
-    values and uoms(units of measure) from ISY. This is how ISY knows what kind
-    of variable to display. Check the UOM's in the WSDK for a complete list.
-    UOM 2 is boolean so the ISY will display 'True/False'
-    """
     drivers = [
         {'driver': 'ST', 'value': 0, 'uom': 2, 'name': "Online"},
         {'driver': 'GV1', 'value': 0, 'uom': 2, 'name': "OnOff"}
     ]
 
-    """
-    id of the node from the nodedefs.xml that is in the profile.zip. This tells
-    the ISY what fields and commands this node has.
-    """
     id = 'Circuit'
 
-    """
-    This is a dictionary of commands. If ISY sends a command to the NodeServer,
-    this tells it which method to call. DON calls setOn, etc.
-    """
     commands = {
         'DON': cmd_on,
         'DOF': cmd_off,
