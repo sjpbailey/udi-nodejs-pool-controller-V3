0f261d767f7c172180d608da0a46c903870024b6
sjpbailey <sjpbailey1961@gmail.com>
2024-02-27 22:24:38 -0800

    Update PoolNode.py

--- nodes/PoolNode.py
+++ nodes/PoolNode.py
@@ -9,40 +9,10 @@ LOGGER = udi_interface.LOGGER
 
 
 class PoolNode(udi_interface.Node):
-    """
-    This is the class that all the Nodes will be represented by. You will
-    add this to Polyglot/ISY with the interface.addNode method.
-
-    Class Variables:
-    self.primary: String address of the parent node.
-    self.address: String address of this Node 14 character limit.
-                  (ISY limitation)
-    self.added: Boolean Confirmed added to ISY
-
-    Class Methods:
-    setDriver('ST', 1, report = True, force = False):
-        This sets the driver 'ST' to 1. If report is False we do not report
-        it to Polyglot/ISY. If force is True, we send a report even if the
-        value hasn't changed.
-    reportDriver(driver, force): report the driver value to Polyglot/ISY if
-        it has changed.  if force is true, send regardless.
-    reportDrivers(): Forces a full update of all drivers to Polyglot/ISY.
-    query(): Called when ISY sends a query request to Polyglot for this
-        specific node
-    """
 
     # self, polyglot, primary, address, name, id, isOn, allDataJson):
     def __init__(self, polyglot, primary, address, name):
-        """
-        Optional.
-        Super runs all the parent class necessities. You do NOT have
-        to override the __init__ method, but if you do, you MUST call super.
 
-        :param polyglot: Reference to the Interface class
-        :param primary: Parent address
-        :param address: This nodes address
-        :param name: This nodes name
-        """
         super(PoolNode, self).__init__(polyglot, primary, address, name)
         self.poly = polyglot
         self.lpfx = '%s:%s' % (address, name)
