ee72b2fa51b0dcb11eb2b059b58a0596d1448c4e
sjpbailey <sjpbailey1961@gmail.com>
2024-03-01 14:31:59 -0800

    Update PoolController.py

--- nodes/PoolController.py
+++ nodes/PoolController.py
@@ -116,7 +116,7 @@ class PoolController(udi_interface.Node):
             # LOGGER.info("Heaters {}".format(self.allDataJson["heaters"]))
             # LOGGER.info("Schedules {}".format(self.allDataJson["schedules"]))
             self.poly.addNode(PoolNode(self.poly, self.address,
-                                       'pooladdr', 'Pool Body', allData))
+                                       'pooladdr', 'Pool Body', allData, self.apiBaseUrl))
 
         for i in self.allDataJson["circuits"]:
             name = i["name"]
