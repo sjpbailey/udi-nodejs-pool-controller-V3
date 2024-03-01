2ba7f834b3618aca9214677625a1d9402f34a7e5
sjpbailey <sjpbailey1961@gmail.com>
2024-03-01 13:32:51 -0800

    Update PoolController.py

--- nodes/PoolController.py
+++ nodes/PoolController.py
@@ -129,8 +129,8 @@ class PoolController(udi_interface.Node):
             self.poly.addNode(SwitchNode(
                 self.poly, self.address, address, name, self.allDataJson))
             # LOGGER.info('Found {} Circuits'.format(len(self.circuits)))
-        self.poly.addNode(BodyNode(
-            self.poly, self.address, "body_1", "Pool Body", self.allDataJson))
+            self.poly.addNode(BodyNode(
+                self.poly, self.address, "body_1", "Pool Body", self.allDataJson))
 
     def delete(self):
         LOGGER.info('Oh God I\'m being deleted. No.')
