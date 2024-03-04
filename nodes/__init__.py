5a55611e1b4ece3527cf3d82c9d99e47c51beded
sjpbailey <sjpbailey1961@gmail.com>
2024-03-04 09:54:35 -0800

    Update __init__.py

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -3,5 +3,6 @@
 
 from .SwitchNode import SwitchNode
 from .PoolNode import PoolNode
+from .PumpNode import PumpNode
 from .TemplateNode import TemplateNode
 from .PoolController import PoolController
